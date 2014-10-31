9a5c0f0544f3aee7cd9b26cfda11bbf70fd04b19
JerryLX <724733136@qq.com>
2014-10-31 21:15:42 +0800

    修改urls & view

--- ourplatform/urls.py
+++ ourplatform/urls.py
@@ -5,4 +5,11 @@ urlpatterns = patterns('',
 #     url(r'^$', views.test, name='index'),
     url(r'^/post/activities/$', views.createActivities, name='createActivities'),
     url(r'^/get/activities/$'. views.getActivities, name='getActivities'),
+    
+    url(r'^/post/users/$'. views.createUser, name='createUser'),
+    url(r'^/get/users/(?P<id>\d+)/$'. views.getUser, name='getUser'),
+    url(r'^/put/users/$'. views.updateUser, name='updateUser'),
+    url(r'^/delete/users/$'. views.deleteUser, name='deleteUser'),
+    url(r'^/login/$'. views.login, name='login'),
+    url(r'^/logout/$'. views.logout, name='logout'),
 )
--- ourplatform/views.py
+++ ourplatform/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.context_processors import request
 from ourplatform.models import *
-from django.http import HttpResponse
+from django.http import *
 from urllib2 import Request
 import json
 
@@ -50,6 +50,118 @@ def getActivityById(request):
                   'description': acbuf.description}
     return HttpResponse(json.dumps(returnData, ensure_ascii=False))
     
+def login(request):
+    payload_json = request.POST['payload']
+    payload = json.load(payload_json)
+    postname = payload['username']
+    postpasswd = payload['password']
+    
+    result = User.objects.filter(username = postname,password = postpasswd)
+    if len(result) == 0:
+        response = HttpResponse()
+        '''ÐÞ¸Ä×´Ì¬Âë'''
+        return response
+    user = result[0]
+    request.session["uid"] = user.id
+    request.session["username"] = user.username
+    request.session["password"] = user.password
+    request.session["gender"] = user.gender
+    return HttpResponse("OK")
+
+def logout(request):
+    try:
+        del request.session['uid']
+        del request.session['username']
+        del request.session['password']
+        del request.session['gender']
+    except KeyError:
+        pass
+    return HttpResponse("OK")
+    
+        
+    
+def createUser(request):
+    payload_json = request.POST['payload']
+    payload = json.load(payload_json)
+    postname = payload['username']
+    postpasswd = payload['password']
+    postgender = payload['gender']
+
+    result = User.objects.filter(username = postname)
+    if len(result)!= 0:
+        return HttpResponseBadRequest('%s has already been registered!' % postname)
+    
+    user = User(postname,postpasswd,postgender)
+    user.save()
+    response = {}
+    response['uid'] = user.id
+    response['username'] = user.username
+    response_json = json.dumps(response)
+    
+    request.session["uid"] = user.id
+    request.session["username"] = user.username
+    request.session["password"] = user.password
+    request.session["gender"] = user.gender
+    
+    return HttpResponse(response_json)
+
+def getUser(request):
+    getid = request.GET['id']
+    user = User.objects.get(id = getid)
+    if len(user) == 0:
+        return HttpResponseBadRequest()
+    response = {}
+    response['uid'] = user.id
+    response['username'] = user.username
+    response['password'] = user.password
+    response['gender'] = user.gender
+    response_json = json.dumps(response)
+    return HttpResponse(response_json)
+
+def updateUser(request):
+    if 'uid' not in request.session:
+        response = HttpResponse()
+        '''modify status'''
+        return response
+    uid = request.session['uid']
+    putid = request.POST['id']
+    if uid != putid:
+        return HttpResponseForbidden()
+    result = User.objects.filter(id = putid)
+    if len(result) == 0:
+        return HttpResponseBadRequest('does not exist')
+    user = result[0]
+    payload_json = request.PUT['payload']
+    payload = json.load(payload_json)
+    user.username = payload['username']
+    user.password = payload['password']
+    user.gender = payload['gender']
+    user.save()
+    return HttpResponse('OK')
+
+def deleteUser(request):
+    if 'uid' not in request.session:
+        response = HttpResponse()
+        '''modify status'''
+        return response
+    uid = request.session['uid']
+    getid = request.POST['id']
+    if uid !=getid:
+        return HttpResponseForbidden()
+    result = User.objects.filter(id = getid)
+    if len(result) == 0:
+        return HttpResponseBadRequest('does not exist')
+    user = result[0]
+    user.delete()
+    try:
+        del request.session['uid']
+        del request.session['username']
+        del request.session['password']
+        del request.session['gender']
+    except KeyError:
+        pass
+    return HttpResponse('OK')
+
     
     
     
