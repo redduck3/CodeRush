937dddc3ebabe6340ce338bcd637fb3062d97f2b
redduck3 <zhouxc3@gmail.com>
2014-11-01 18:38:43 +0800

    finish login register

--- ourplatform/static/js/app.js
+++ ourplatform/static/js/app.js
@@ -10,4 +10,36 @@ angular.module('tongji', [
     $routeProvider.when('/login', {
         templateUrl: '/static/views/login.html'
     });
+
+    $routeProvider.when('/register', {
+        templateUrl: '/static/views/register.html'
+    });
 }])
+
+.factory('pregMatchService', function() {
+    var service = {
+        pregMatch: function (name, value, displayName) {
+            //若不存在，在默认为无需匹配
+            if (typeof service.regExpression[name] === 'undefined') {
+                return true;
+            }
+            if (value.toString().match(service.regExpression[name]) !== null) {
+                return true;
+            } else {
+                service.error = "请输入正确的" + displayName;
+                return false;
+            }
+        },
+        regExpression : {
+            email: /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/,
+            student_number: /^[0-9]+$/,
+            phone: /^1[3|4|5|8]\d{9}$/,
+            chinese: /^[\u4E00-\u9FA5]+$/,
+            identity: /^[A-Za-z0-9_]+$/
+        },
+        error : ""
+    };
+
+    return service;
+})
+
